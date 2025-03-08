586679f4f85f1851d77bbfa5a105ed8b9f399ae9
jaronlong52 <longjm3@appstate.edu>
2025-03-08 13:13:01 -0500

    cleaning up manual auth and installing nextauth and bcrypt

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
+import { prisma } from "@/lib/database";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+	/* config options here */
+	serverExternalPackages: ["@prisma/client", "bcrypt"],
 };
 
 export default nextConfig;
