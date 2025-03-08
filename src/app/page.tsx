dba7c078c91f5e0eddf3e119545c00657033cb4d
jaronlong52 <longjm3@appstate.edu>
2025-03-08 17:44:08 -0500

    opening page to select sign in or sign up

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { LoginButton, LogoutButton } from "./auth";
+import { LoginButton, LogoutButton } from "./auth/auth";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default async function Home() {
-	const session = await getServerSession(authOptions);
-
 	return (
-		<main>
-			<LoginButton />
-			<LogoutButton />
-			{session ? <p>Authenticated</p> : <p>Not authenticated</p>}
+		<main className="flex items-center justify-center h-screen w-screen bg-slate-100">
+			<div className="bg-white shadow-xl rounded-lg p-6 max-w-md w-full -mt-40 transform scale-150">
+				<div className="mb-4">
+					<h2 className="text-5xl font-bold text-center p-5">
+						Scheduler
+					</h2>
+				</div>
+				<div className="flex justify-center space-x-6">
+					<LoginButton />
+					<Button size="lg">
+						<Link className="text-lg" href="/register">
+							Sign Up
+						</Link>
+					</Button>
+				</div>
+			</div>
 		</main>
 	);
 }
