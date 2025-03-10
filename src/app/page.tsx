a0213fb8c7b7332600e2c490be94727699a855bf
jaronlong52 <longjm3@appstate.edu>
2025-03-10 15:10:52 -0400

    user routed to home page if already signed in using redirect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,22 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
-import { LoginButton, LogoutButton } from "./auth/auth";
+import { LoginButton } from "./auth/auth";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
+	const session = await getServerSession(authOptions);
+
+	if (session) {
+		redirect("/home");
+	}
+
 	return (
 		<main className="flex items-center justify-center h-screen w-screen bg-slate-100">
 			<div className="bg-white shadow-xl rounded-lg p-6 max-w-md w-full -mt-40 transform scale-150">
 				<div className="mb-4">
-					<h2 className="text-5xl font-bold text-center p-5">
-						Scheduler
-					</h2>
+					<h2 className="text-5xl font-bold text-center p-5">Scheduler</h2>
 				</div>
 				<div className="flex justify-center space-x-6">
 					<LoginButton />
